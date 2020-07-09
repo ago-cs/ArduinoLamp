d0389d630ca700873309dcd63c54a355aa2254d2
Stepko <sguralmen@gmail.com>
2020-07-09 22:09:53 +0300

    Update GyverLamp_v1.4_Arduino_v1.3.1.ino

--- GyverLamp_v1.4_Arduino_v1.3.1/GyverLamp_v1.4_Arduino_v1.3.1.ino
+++ GyverLamp_v1.4_Arduino_v1.3.1/GyverLamp_v1.4_Arduino_v1.3.1.ino
@@ -42,7 +42,7 @@
 // ============= ДЛЯ РАЗРАБОТЧИКОВ =============
 #define LED_PIN 2             // пин ленты
 #define BTN_PIN 4
-#define MODE_AMOUNT 20
+#define MODE_AMOUNT 21
 #define NUM_LEDS WIDTH * HEIGHT
 #define SEGMENTS 1            // диодов в одном "пикселе" (для создания матрицы из кусков ленты)
 
